60516179e314f9e30dbd0975790b73a2ae3ff81c
TaylanTorres09 <taylantorres@gmail.com>
2023-07-17 13:02:28 -0300

    method findByContaID

--- src/services/service.js
+++ src/services/service.js
@@ -2,12 +2,17 @@
 
 const url = import.meta.env.VITE_URL;
 
-const findByIdConta = async () => {
-
+const findByIdConta = async (id) => {
+    const response = await fetch(`${url}transferencia?contaId=${id}`, {
+        method: 'GET',
+        mode: 'cors',
+    });
+    const data = await response.json();
+    return data;
 }
 
 const findAll = async () => {
-    const response = await fetch(`${url}transferencia/all`, {
+    const response = await fetch(`${url}/transferencia/all`, {
         method: 'GET',
         mode: 'cors',
     });
