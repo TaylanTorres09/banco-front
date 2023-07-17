f20de6e89ec6a4a8f83aa10869bc7597cef47475
TaylanTorres09 <taylantorres@gmail.com>
2023-07-17 11:04:29 -0300

    Configure dotEnv

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+dotenv.config().parsed;
 
 // https://vitejs.dev/config/
 export default defineConfig({
