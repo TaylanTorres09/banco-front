613f810acb0eefb76353f80f39de39dcf73c80c1
TaylanTorres09 <taylantorres@gmail.com>
2023-07-17 15:13:04 -0300

    style dates

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,9 +15,12 @@ const Table = ({ data }) => {
             <tbody>
                 {
                     data.map((d) => {
+                        const date = new Date(d.dataTransferencia);
+                        const day = date.getDate() > 10 ? `${date.getDate()}` : `0${date.getDate()}`;
+                        const month = date.getMonth()+1 > 10 ? `${date.getMonth()+1}` : `0${date.getMonth()+1}`;
                         return (
                             <tr key={d.id}>
-                                <td>{d.dataTransferencia}</td>
+                                <td>{`${day}/${month}/${date.getFullYear()}`}</td>
                                 <td>R$ {d.valor}</td>
                                 <td>{d.tipo}</td>
                                 <td>{d.nomeOperadorTransacao ? d.nomeOperadorTransacao : "----"}</td>
